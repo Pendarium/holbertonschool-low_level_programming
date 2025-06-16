3dddfcd08666b1b0c7250847ed3f2e8c03e9b4e9
Pendarium <thomas.sorli21@gmail.com>
2025-06-16 16:37:45 +0200

    try

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,8 +8,15 @@
 */
 int main(void)
 {
+int i;
+double d;
+char c;
 
-
+printf("Size of a char: %lu\n", sizeof(c));
+printf("Size of an int: \n %lu", sizeof(i));
+printf("Size of a long int: \n %lu", sizeof(i));
+printf("Size of a long long int: \n %lu", sizeof(d));
+printf("Size of a float: \n %lu", sizeof(i));
 
 return (0);
 }
