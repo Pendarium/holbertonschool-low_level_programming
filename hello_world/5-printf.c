5e2c89ad83a1fb6e6e0f45c7b4d36e6cf690b271
Pendarium <thomas.sorli21@gmail.com>
2025-06-16 15:33:39 +0200

    printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,25 +2,14 @@
 /**
 *main - Point d'entrée du programme
 *
-*Description:Cette fonction est le point de départ du programme.
+*Description: Cette fonction est le point de départ du programme.
 *
 *Return: 0 si le programme se termine correctement
-*/
-/**
-*printf - affiche une chaîne formatée sur la sortie standard
-*@format: chaîne de format contenant le texte et les spécificateurs
-*
-*Description:
-*Cette fonction affiche une chaîne de caractères formatée
-*sur la sortie standard (l’écran). Elle remplace les
-*spécificateurs comme %s, %d, %c, etc. par les arguments
-*correspondants.
-*
-*Retour : Le nombre de caractères affichés.
 */
 int main(void)
 {
 
+/* printf = imprime sur std out */
 printf("with proper grammar, but the outcome is a piece of art,\n");
 
 return (0);
