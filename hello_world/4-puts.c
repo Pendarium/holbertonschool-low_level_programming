ae5bbf3dd312de4cdc029c5c147471b9824d9201
Pendarium <thomas.sorli21@gmail.com>
2025-06-16 14:00:09 +0200

    desc up of fonction

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 
-int main(void)
-
 /**
  * main - Point d'entrée du programme
  *
  * Description: Cette fonction est le point de départ du programme.
- * Elle exécute les instructions principales et retourne 0 si tout
- * s'est bien passé.
+ * Elle exécute les instructions principales et retourne 0 si tout s'est bien passé.
  *
  * Return: 0 si le programme se termine correctement
  */
+ 
+int main()
 {
-
-puts("\"Programming is like building a multilingual puzzle");
-
+puts(' Programming is like building a multilingual puzzle' );
 
 return (0);
-
-
 
 }
