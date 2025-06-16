eb028d159b1afa6a621662d165aa31ae2deb5374
Pendarium <thomas.sorli21@gmail.com>
2025-06-16 14:13:22 +0200

    puts try 5934

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,12 +3,12 @@
  * main - Point d'entrée du programme
  *
  * Description: Cette fonction est le point de départ du programme.
- * Elle exécute les instructions principales et retourne 0 si tout s'est bien passé.
  *
  * Return: 0 si le programme se termine correctement
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
 }
