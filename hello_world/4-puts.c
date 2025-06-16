1d5aea8fc97425326b8fc00d1ca9ecb552cb7f49
Pendarium <thomas.sorli21@gmail.com>
2025-06-16 12:31:07 +0200

    ...

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
-/*
-*#include <stdio.h> = ajouter a la bibliotheque standard
-*/
+    /*
+    *#include <stdio.h> = ajouter a la bibliotheque standard
+    */
 int main(void)
-/*
-*int main = (point de depart)
-*/
+    /*
+    *int main = (point de depart)
+    */
 {
 puts("\"Programming is like building a multilingual puzzle");
-
-/*
-*puts = permet d'afficher du texte avec \n automatique a la fin
-*/
+    /*
+    *puts = permet d'afficher du texte avec \n automatique a la fin
+    */
 
 return (0);
 
-/*
-*reviens a 0
-*/
+    /*
+    *reviens a 0
+    */
 
 }
