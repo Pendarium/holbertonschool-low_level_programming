b1bebcd303749ea3f468b920b8324e67d7678d8a
Pendarium <thomas.sorli21@gmail.com>
2025-06-16 14:22:20 +0200

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+/**
+* printf - imprime texte
+*
+* Description: Cette fonction sert a imprimé sans retour a la ligne.
+*
+*/
+printf( "with proper grammar, but the outcome is a piece of art,\n" );
+
+return (0);
+}
