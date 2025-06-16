5e7c1bc5fb19a754dd477b181a5c0a35058db1a7
Pendarium <thomas.sorli21@gmail.com>
2025-06-16 15:50:12 +0200

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - Point d'entrée du programme
+*
+*Description: Cette fonction est le point de départ du programme.
+*
+*Return: 0 si le programme se termine correctement
+*/
+int main(void)
+{
+
+
+
+return (0);
+}
