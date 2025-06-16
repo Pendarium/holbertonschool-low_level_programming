cf87ab86905b21cd2498bbdda9df24074622b3a7
Pendarium <thomas.sorli21@gmail.com>
2025-06-16 17:03:07 +0200

    try 5679

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,14 +9,16 @@
 int main(void)
 {
 int i;
-double d;
+float f;
 char c;
+int long il;
+int long long ill;
 
-printf("Size of a char: %lu\n", sizeof(c));
-printf("Size of an int: %lu\n", sizeof(i));
-printf("Size of a long int: %lu\n", sizeof(i));
-printf("Size of a long long int: %lu\n", sizeof(d));
-printf("Size of a float: %lu\n", sizeof(i));
+printf("Size of a char: %lu byte(s)\n", sizeof(c));
+printf("Size of an int: %lu byte(s)\n", sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", sizeof(il));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(ill));
+printf("Size of a float: %lu byte(s)\n", sizeof(f));
 
 return (0);
 }
