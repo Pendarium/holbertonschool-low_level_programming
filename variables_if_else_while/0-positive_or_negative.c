d3c9625e432505a3ee88fdaf79dd5105747bb6ee
Pendarium <thomas.sorli21@gmail.com>
2025-06-17 10:08:15 +0200

    try 1

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Point d'entrée du programme
+*
+*Description: Cette fonction est le point de départ du programme.
+*
+*Return: 0 si le programme se termine correctement
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+    {
+        printf("%d is positive\n", n);
+    }
+    else if (n < 0)
+    {
+        printf("%d is negative\n", n);
+    }
+    else (n == 0)
+    {
+        printf("%d is equal\n", n);
+    }
+	return (0);
+}
