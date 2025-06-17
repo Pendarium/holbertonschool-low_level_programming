d0ba0c8468548f1387e1e06ae4280f062d783e2c
Pendarium <thomas.sorli21@gmail.com>
2025-06-17 16:22:37 +0200

    try

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -10,12 +10,13 @@ int main(void)
 {
 int s;
 
-for (s = '0'; s <= '9'; s++)
+for (s = 0; s <= 9; s++)
 {
-printf("%d\n", s);
+    printf("%d", s);
 }
-putchar(s);
+
 putchar('\n');
+
 
 return (0);
 }
