8845709446c06cd4e44e1a4cdc5d5fb2d359f358
Pendarium <thomas.sorli21@gmail.com>
2025-06-17 16:04:37 +0200

    try 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Point d'entrée du programme
+*
+*Description: Cette fonction est le point de départ du programme.
+*
+*Return: 0 si le programme se termine correctement
+*/
+int main(void)
+{
+int s;
+
+for (s = '0'; s <= '9'; s++)
+{
+printf("%d\n", s);
+}
+return (0);
+}
