0a4f00081e1e84fea353721ae7d4041ec8420e60
Pendarium <thomas.sorli21@gmail.com>
2025-06-17 14:30:37 +0200

    try print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Point d'entrée du programme
+*
+*Description: Cette fonction est le point de départ du programme.
+*
+*Return: 0 si le programme se termine correctement
+*/
+int main(void)
+{
+int az;
+
+for (az = 'a'; az <= 'z'; az++)
+putchar(az)
+
+putchar('\n')
+}
