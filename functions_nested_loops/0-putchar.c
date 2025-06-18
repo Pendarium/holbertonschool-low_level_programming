72bb2c7d2168192b9a98919a5249e4647846072f
Pendarium <thomas.sorli21@gmail.com>
2025-06-18 14:41:33 +0200

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+*main - Point d'entrée du programme
+*
+*Description: Cette fonction est le point de départ du programme.
+*
+*Return: 0 si le programme se termine correctement
+*/
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+
+return (0);
+}
