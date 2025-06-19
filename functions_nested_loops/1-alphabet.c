141169342be38a0cc978b4fa480cceaa3904ff89
Pendarium <thomas.sorli21@gmail.com>
2025-06-19 10:14:55 +0200

    try

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*main - Point d'entrée du programme
+*
+*Description: Cette fonction est le point de départ du programme.
+*
+*Return: 0 si le programme se termine correctement
+*/
+int main(void)
+{
+
+int az;
+
+for (az = 'a'; az <= 'z'; az++)
+{
+_putchar(az);
+}
+_putchar('\n');
+
+return (0);
+}
