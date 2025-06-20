4d1ca97b8cbad5535c25a17bb47fe8246b4b69cd
Pendarium <thomas.sorli21@gmail.com>
2025-06-20 14:58:23 +0200

    betty

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,20 @@
 #include "main.h"
 /**
-* _abs - Vérifie si un caractère est une lettre minuscule.
-* @n: Le caractère à tester (en code ASCII).
+* print_last_digit - Vérifie si un caractère est une lettre minuscule.
+* @l: Le caractère à tester (en code ASCII).
 *
 *Return: n si le caractère est une lettre minuscule (entre 'a' et 'z').
 *
 */
-int print_last_digit(int)
+int print_last_digit(int l)
 {
+int lastDigit = l % 10;
+
+if (lastDigit < 0)
+{
+lastDigit = -lastDigit;
+}
+_putchar(lastDigit + '0');
+return (lastDigit);
 
 }
