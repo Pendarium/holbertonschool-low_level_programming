acebac34cd2a976dd800fb620ff6a39d79c53cd5
Pendarium <thomas.sorli21@gmail.com>
2025-06-20 14:49:27 +0200

    main

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _abs - Vérifie si un caractère est une lettre minuscule.
+* @n: Le caractère à tester (en code ASCII).
+*
+*Return: n si le caractère est une lettre minuscule (entre 'a' et 'z').
+*
+*/
+int print_last_digit(int)
+{
+
+}
