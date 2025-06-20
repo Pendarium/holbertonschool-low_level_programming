c34ffc57ed0e70af193813c21706934acf5193d5
Pendarium <thomas.sorli21@gmail.com>
2025-06-20 11:55:32 +0200

    try

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_sign - Vérifie si un caractère est une lettre minuscule.
+* @n: Le caractère à tester (en code ASCII).
+*
+*Return: 1 si le caractère est une lettre minuscule (entre 'a' et 'z').
+*
+*/
+int _abs(int n)
+{
+
+if (n < 0)
+return (-1);
+else
+return (0);
+}
