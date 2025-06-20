6eacc63f4fe73ef9a4aa8c16c2fd9c1884158532
Pendarium <thomas.sorli21@gmail.com>
2025-06-20 10:55:26 +0200

    try 2

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-* _isalpha - Vérifie si un caractère est une lettre minuscule.
-* @c: Le caractère à tester (en code ASCII).
+* print_sign - Vérifie si un caractère est une lettre minuscule.
+* @n: Le caractère à tester (en code ASCII).
 *
 *Return: 1 si le caractère est une lettre minuscule (entre 'a' et 'z').
 *
