4d413e5ddd78925603487195a754fa1a0587347b
Pendarium <thomas.sorli21@gmail.com>
2025-06-23 09:32:32 +0200

    start proj1

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+*print_alphabet - Point d'entrée du programme
+*
+*Description: Cette fonction est le point de départ du programme.
+*
+*Return: 0 si le programme se termine correctement
+*/
+int _isupper(int c)
