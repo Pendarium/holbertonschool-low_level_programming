27d96affe27853968a9b74cc7157ad985c21acbe
Pendarium <thomas.sorli21@gmail.com>
2025-06-23 10:18:08 +0200

    for betty

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_numbers - Point d'entrée du programme
+*
+*Description: Cette fonction est le point de départ du programme.
+*
+*Return: 0 si le programme se termine correctement
+*/
+void print_numbers(void)
+{
+
+int n;
+
+for (n = '0'; n <= 9; n++)
+_putchar (n);
+_putchar ('\n');
+}
