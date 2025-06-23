7053e5ed4bb699e4c2be842f569e40076d554811
Pendarium <thomas.sorli21@gmail.com>
2025-06-23 09:44:46 +0200

    0 try

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
 /**
-*print_alphabet - Point d'entrée du programme
+*_isupper - Point d'entrée du programme
 *
 *Description: Cette fonction est le point de départ du programme.
-*
+*@c:ekzokfisk,q
 *Return: 0 si le programme se termine correctement
 */
 int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
