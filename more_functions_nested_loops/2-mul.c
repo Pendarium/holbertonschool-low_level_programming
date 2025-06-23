b17fe4800256f3ef1a50d3bc31c2c5242830babd
Pendarium <thomas.sorli21@gmail.com>
2025-06-23 10:08:30 +0200

    ...

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*mul - Point d'entrée du programme
+*
+*Description: Cette fonction est le point de départ du programme.
+*@a:ekzokfisk,q
+*@b:,fopiezk,drsov;,
+*Return: 0 si le programme se termine correctement
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
