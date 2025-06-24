ec21d0f864b565e4c48c95b6d2372d7b1932f683
Pendarium <thomas.sorli21@gmail.com>
2025-06-24 11:35:04 +0200

    try

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_square - Entry point
+* @:jiohbvrf
+*
+*Écrivez une fonction qui imprime la table de multiplication par 9,
+*en commençant par 0.
+*
+*return (0)
+*/
+
+void reset_to_98(int *n)
+{
+int n = 98;
+int *n = &n;
+}
