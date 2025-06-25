6c0453082315d52d5172b4a3eff65c6ea765fe9e
Pendarium <thomas.sorli21@gmail.com>
2025-06-25 14:27:42 +0200

    try 2

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,24 @@
 #include "main.h"
 /**
+ * _strlen - Calcule la longueur d'une chaîne de caractères
+ * @s: pointeur vers la chaîne de caractères à mesurer
+ *
+ * Return: la longueur de la chaîne (sans compter le caractère nul),
+ *         ou 0 si le pointeur est NULL
+ */
+int _strlen(char *s)
+{
+char *p;
+if (s == NULL)
+return (0);
+
+p = s;
+while (*p != '\0')
+p++;
+return (p - s);
+}
+
+/**
 * puts_half - Inverse une chaîne de caractères
 *
 *@str:coijaecio
