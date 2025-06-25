9050806c339d3074ed1601e3e350f7142240b727
Pendarium <thomas.sorli21@gmail.com>
2025-06-25 15:26:23 +0200

    betty

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,16 @@
 #include "main.h"
-
+/**
+*print_array - entry
+*@a:pzvieqoj,
+*@n:pzvieqoj,
+*Écrivez une fonction qui affiche la moitié d'une chaîne,
+*suivie d'un saut de ligne.
+*
+*La fonction doit afficher la seconde moitié de la chaîne.
+*Si le nombre de caractères est impair, la fonction doit
+*afficher les n derniers caractères de la chaîne,
+*où n = (length_of_the_string + 1) / 2
+*/
 
 void print_array(int *a, int n)
 {
