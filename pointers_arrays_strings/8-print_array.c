8438ddd37a342ab7b515fc8b4d3c685918196303
Pendarium <thomas.sorli21@gmail.com>
2025-06-25 15:33:04 +0200

    commentaire

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,28 +1,33 @@
 #include "main.h"
 /**
-*print_array - entry
-*@a:pzvieqoj,
-*@n:pzvieqoj,
-*Écrivez une fonction qui affiche la moitié d'une chaîne,
-*suivie d'un saut de ligne.
+* print_array - Affiche les n premiers éléments d’un tableau d’entiers
+* @a: pointeur vers le tableau d'entiers
+* @n: nombre d'éléments du tableau à afficher
 *
-*La fonction doit afficher la seconde moitié de la chaîne.
-*Si le nombre de caractères est impair, la fonction doit
-*afficher les n derniers caractères de la chaîne,
-*où n = (length_of_the_string + 1) / 2
+* Affiche les n premiers éléments du tableau séparés par des virgules
+* et un espace, sauf après le dernier élément. Termine par un saut de ligne.
 */
 
 void print_array(int *a, int n)
+/* Définition de la fonction : */
+/*prend un tableau d'entiers et le nombre d'éléments à afficher*/
 {
 int i;
+/* Déclaration de la variable de boucle*/
+
 for (i = 0; i < n; i++)
+/* Boucle pour parcourir les n premiers éléments du tableau*/
 {
 printf("%d", a[i]);
+/* Affiche l'élément i du tableau sans espace ni saut de ligne*/
 
 if (i < n - 1)
+/* Si ce n'est pas le dernier élément*/
 {
 printf(", ");
+/*Affiche une virgule et un espace pour séparer les éléments*/
 }
 }
 printf("\n");
+/* Affiche un saut de ligne à la fin de l'affichage*/
 }
