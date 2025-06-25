4be9005fbaac79b67e647fc6a0b975c4b9a3af88
Pendarium <thomas.sorli21@gmail.com>
2025-06-25 16:33:04 +0200

    comentaire 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/**
+
+/*
  *_strcpy - Copie la chaîne pointée par src y compris le caractère nul '\0',
  *          vers le tampon pointé par dest.
  * @dest: Pointeur vers le tableau dans lequel la chaîne sera copiée.
@@ -11,11 +12,17 @@
 char *_strcpy(char *dest, char *src)
 {
 int i = 0;
+/* Initialise un compteur i à 0 pour l'utilisation dans la boucle */
 while (src[i] != '\0')
+/* Parcourt chaque caractère de src jusqu'au caractère nul */
 {
 dest[i] = src[i];
+/* Copie le caractère courant de src vers dest */
 i++;
+/* Incrémente l'indice pour passer au caractère suivant */
 }
 dest[i] = '\0';
+/* Ajoute le caractère nul à la fin de la chaîne copiée */
 return (dest);
+/* Retourne le pointeur vers la destination */
 }
