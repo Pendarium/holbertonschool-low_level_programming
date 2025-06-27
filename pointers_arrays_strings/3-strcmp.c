a629b2616a9cd0510ce90510709f473a8584d72b
Pendarium <thomas.sorli21@gmail.com>
2025-06-27 10:13:40 +0200

    strcmp updt

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -13,11 +13,11 @@ int _strcmp(char *s1, char *s2)
 {
 /* Boucle tant que les caractères sont égaux et non terminés */
 while (*s1 != '\0' && *s1 == *s2)
-{
-/* Avance dans les deux chaînes */
-s1++;
-s2++;
-}
+	{
+	/* Avance dans les deux chaînes */
+	s1++;
+	s2++;
+	}
 /* Retourne la différence entre les caractères ASCII divergents */
-return (*s1 - *s2);
+	return (*s1 - *s2);
 }
