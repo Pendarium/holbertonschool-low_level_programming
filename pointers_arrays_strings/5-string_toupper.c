c090c9446f055f830dbea2864feee02b008d93b7
Pendarium <thomas.sorli21@gmail.com>
2025-06-27 11:48:00 +0200

    string up

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,29 @@
+include "main.h"
+/**
+* string_toupper - Convertit toutes les lettres minuscules d’une chaîne
+*
+* @str: Pointeur vers la chaîne de caractères à convertir
+*
+* Return: Pointeur vers la chaîne convertie en majuscules
+*/
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+	/* Initialise l'indice à 0 */
+
+	while (str[i] != '\0')
+	/* Parcourt la chaîne jusqu'à la fin */
+	{
+		if (str[i] >= ('a') && str[i] <= 'z')
+		/* Si caractère minuscule */
+		{
+			str[i] -= 32;
+			/* Convertit en majuscule en soustrayant 32 */
+		}
+		i++;
+		/* Passe au caractère suivant */
+	}
+	return (str);
+	/* Retourne le pointeur vers la chaîne modifiée */
+}
