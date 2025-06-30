7f4972d6a2a32ba24ccc3c2998a98000c65050f6
Pendarium <thomas.sorli21@gmail.com>
2025-06-30 16:07:28 +0200

    strstr updt

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,30 +1,39 @@
 #include "main.h"
-/**
- * _strstr - Copie n octets depuis la source vers la destination
- * @haystack: pointeur vers la zone de destination
- * @needle: pointeur vers la zone source
- * @: nombre d'octets à copier
- *
- * Return: pointeur vers la mémoire remplie (s)
- */
-char *_strstr(char *haystack, char *needle)
 
+/**
+ * _strstr - cherche la première occurrence de needle dans haystack
+ * @haystack: chaîne principale dans laquelle chercher
+ * @needle: sous-chaîne à rechercher dans haystack
+ *
+ * Return: pointeur vers le début de la sous-chaîne trouvée,
+ *         ou NULL si needle n'est pas trouvée dans haystack
+ */
+
+char *_strstr(char *haystack, char *needle)
 {
 	int i;
+	/* Index pour parcourir needle */
 
 	if (*needle == '\0')
+		/* Si needle est une chaîne vide, on retourne haystack */
 		return (haystack);
 
 	while (*haystack)
+		/* Tant que le caractère courant de haystack n'est pas nul */
 	{
 		for (i = 0; needle[i]; i++)
+			/* Parcourt needle tant que le caractère n'est pas nul */
 		{
 			if (haystack[i] != needle[i])
+				/* Si les caractères ne correspondent pas, on arrête */
 				break;
 		}
 		if (needle[i] == '\0')
+			/* Si on a atteint la fin de needle, correspondance trouvée */
 			return (haystack);
 		haystack++;
+		/* Passe au caractère suivant dans haystack */
 	}
 	return (NULL);
+	/* Aucun match trouvé, on retourne NULL */
 }
