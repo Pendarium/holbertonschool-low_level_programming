5de059d6e7f23e803e22e993a751778d902595fa
Pendarium <thomas.sorli21@gmail.com>
2025-06-30 14:30:52 +0200

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,22 +1,49 @@
 #include "main.h"
-
+/**
+ * _strspn - retourne la longueur du préfixe de s composé uniquement
+ *           de caractères présents dans accept
+ * @s: chaîne source à analyser
+ * @accept: chaîne contenant les caractères acceptés
+ * Return: longueur du préfixe de s composé de caractères dans accept
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-   int i, j;
-
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-			if (s[i] == accept[j])
-			{
-				return(j + 2);
-			}
-			if (s[i] == '\0')
-			{
-				return (i);
-			}
+int index;
+	/* Variable pour parcourir la chaîne s */
+int sec;
+	/* Variable pour parcourir la chaîne accept */
+int count = 0;
+	/* Compteur du nombre de caractères du préfixe */
+for (index = 0; s[index] != '\0'; index++)
+	/* Parcourt chaque caractère de s jusqu'à la fin de la chaîne */
+{
+	int match = 0;
+	/* Indique si une correspondance est trouvée dans accept */
+	for (sec = 0; accept[sec] != '\0'; sec++)
+		/* Parcourt chaque caractère de accept */
+	{
+		if (s[index] == accept[sec])
+			/* Vérifie la correspondance entre s et accept */
+		{
+			match = 1;
+			/* Marque qu'une correspondance est trouvée */
+			break;
+			/* Quitte la boucle une fois la correspondance trouvée */
 		}
 	}
-	return(i);
+	if (match)
+		/* Si une correspondance est trouvée */
+	{
+		count++;
+		/* Incrémente le compteur */
+	}
+	else
+		/* Si aucune correspondance trouvée */
+	{
+		break;
+		/* Quitte la boucle principale */
+	}
+}
+return (count);
+	/* Retourne la longueur du préfixe */
 }
