468842f583ee70ba64d670ec0d44a8b9974dbd11
Pendarium <thomas.sorli21@gmail.com>
2025-06-30 11:44:06 +0200

    triche ou pas

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -20,6 +20,7 @@ int main(void)
  *
  * Return: Always 0.
  */
+/*
 int main(void)
 {
     char s1[] = "Hello";
@@ -28,5 +29,21 @@ int main(void)
     printf("%d\n", _strcmp(s1, s2));
     printf("%d\n", _strcmp(s2, s1));
     printf("%d\n", _strcmp(s1, s1));
+    return (0);
+}
+*/
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
+
+    n = _strspn(s, f);
+    printf("%u\n", n);
     return (0);
 }
