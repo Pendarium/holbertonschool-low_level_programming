51de6d0e13dd7e1784484feaf11ca0cbd682844a
Pendarium <thomas.sorli21@gmail.com>
2025-07-01 11:16:40 +0200

    puts recurssive

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,12 +1,23 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - Affiche une chaîne de caractères et d’un saut de ligne
+ * @s: Chaîne de caractères à afficher
+ * Return: Rien
+ */
+
 void _puts_recursion(char *s)
 {
-int c;
-
-for (c = 0; s[c] != '\0'; c++)
-{
-_putchar(s[c]);
-}
-_putchar('\n');
+	if (*s == '\0')
+	/* Si on atteint la fin de la chaîne */
+	{
+		_putchar('\n');
+		/* Affiche un saut de ligne */
+		return;
+		/* Termine la fonction */
+	}
+	_putchar(*s);
+	/* Affiche le caractère courant */
+	_puts_recursion(s + 1);
+	/* Appelle la fonction pour le caractère suivant */
 }
