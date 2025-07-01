68cb7a5f1693a28065aac942504153794904f50b
Pendarium <thomas.sorli21@gmail.com>
2025-07-01 17:08:53 +0200

    coment

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,29 +1,35 @@
 #include "main.h"
 
 /**
- * leet - replaces some char by int
- * @str: string to modify
- * Return: A pointer to the modified string
+ * leet - remplace certains caractères par des chiffres
+ * @str: chaîne de caractères à modifier
+ * 
+ * Return: un pointeur vers la chaîne modifiée
  */
-/*
 char *leet(char *str)
 {
 	int i;
 	int j;
 	char leetn[] = "aAeEoOtTlL";
+	/* caractères à remplacer */
 	char rep[] = "4433007711";
+	/* chiffres de remplacement */
 
 	for (i = 0; str[i] != '\0'; i++)
+	/* parcourt chaque caractère de str */
 	{
 		for (j = 0; leetn[j] != '\0'; j++)
+		/* parcourt les caractères à remplacer */
 		{
 			if (str[i] == leetn[j])
+			/* si caractère trouvé dans leetn */
 			{
 				str[i] = rep[j];
+				/* remplace par le chiffre correspondant */
 			}
 		}
 	}
 
 	return (str);
+	/* retourne la chaîne modifiée */
 }
-*/
