63453b7db7d51eccbbdd9899867c7b5d300a2990
Pendarium <thomas.sorli21@gmail.com>
2025-07-02 15:44:56 +0200

    mynameis updt

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,11 +1,24 @@
 #include "main.h"
 
+/**
+ * main - Affiche tous les arguments reçus par le programme
+ * @argc: Nombre d'arguments passés au programme
+ * @argv: Tableau contenant les arguments sous forme de chaînes
+ *
+ * Return: 0 si le programme s'exécute correctement
+ */
 int main(int argc, char *argv[])
+	/* Fonction principale, prend le nb d'arguments et leurs valeurs */
 {
 	int i;
-for (i = 0; i < argc; i++)
+	/* Déclaration de la variable i pour la boucle */
+
+	for (i = 0; i < argc; i++)
+	/* Boucle pour parcourir tous les arguments */
 	{
 		puts(argv[i]);
+	/* Affiche chaque argument sur une ligne distincte */
 	}
 	return (0);
+	/* Retourne 0 pour indiquer que le programme s'est bien terminé */
 }
