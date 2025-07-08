0c85c367769483d7fe158aee6cadfd8b2a60c6eb
Pendarium <thomas.sorli21@gmail.com>
2025-07-08 10:05:15 +0200

    malloc updt

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,14 +1,32 @@
 #include"main.h"
 
+/**
+ * malloc_checked - Alloue de la mémoire dynamiquement
+ * @b: taille en octets à allouer
+ *
+ * Description: Cette fonction alloue de la mémoire de
+ *				taille 'b' à l'aide de malloc.
+ * Si l'allocation échoue, le programme termine avec le code de sortie 98.
+ *
+ *
+ * Return: un pointeur vers la mémoire allouée.
+ */
+
 void *malloc_checked(unsigned int b)
 {
 	int *size;
+	/* Déclare un pointeur pour stocker l'adresse allouée */
 
 	size = malloc(b);
+	/* Alloue b octets de mémoire dynamiquement */
+
 	if (size == NULL)
+	/* Vérifie si l'allocation a échoué */
+
 	{
 		exit(98);
+		/* Quitte le programme avec le code d'erreur 98 */
 	}
-	return(size);
-
+	return (size);
+	/* Retourne le pointeur vers la mémoire allouée */
 }
