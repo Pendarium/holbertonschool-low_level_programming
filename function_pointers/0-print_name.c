5eba46251cbb83fc01988316d55387b4e92482f8
Pendarium <thomas.sorli21@gmail.com>
2025-07-15 10:10:38 +0200

    try

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -6,7 +6,7 @@
  * @f: Pointeur vers la fonction à appliquer sur le nom
  *
  * Description: Cette fonction appelle la fonction pointée par @f
- *              en lui passant @name comme argument.
+ * en lui passant @name comme argument.
  */
 
 void print_name(char *name, void (*f)(char *))
