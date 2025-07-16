855a8e8fb9dbdfb7233d6ff649b93bae1466a188
Pendarium <thomas.sorli21@gmail.com>
2025-07-16 16:39:04 +0200

    print number

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,43 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Affiche des nombres suivis d’un séparateur
+ * @separator: Chaîne à afficher entre les nombres
+ * @n: Nombre d'entiers à afficher
+ * Return: Rien
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+	unsigned int index;
+	/* Compteur de boucle */
+
+	va_list args;
+	/* Déclaration de la liste d'arguments */
+
+	if (separator == NULL)
+	return;
+	/* Si le séparateur est NULL, ne rien faire */
+
+	va_start(args, n);
+	/* Initialisation de la liste */
+
+	for (index = 0; index < n; index++)
+		{
+		printf("%d", va_arg(args, int));
+		/* Affiche l'entier suivant dans la liste */
+
+			if (index < n - 1)
+			{
+			printf("%s", separator);
+			/* Affiche le séparateur sauf après le dernier nombre */
+			}
+		}
+
+	printf("\n");
+	/* Nouvelle ligne à la fin de l'affichage */
+
+	va_end(args);
+	/* Termine l'utilisation de la liste */
+}
