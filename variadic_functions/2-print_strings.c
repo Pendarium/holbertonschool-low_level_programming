8ca156a5302d06c6d3a32985bb725bc0dbf0a22c
Pendarium <thomas.sorli21@gmail.com>
2025-07-18 14:49:14 +0200

    BETTY

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,43 +1,54 @@
 #include "variadic_functions.h"
+/* Inclusion de l'en-tête pour les fonctions */
+
+/**
+ * print_strings - Affiche des chaînes de caractères séparées
+ * @separator: Chaîne utilisée pour séparer les chaînes affichées
+ * @n: Nombre de chaînes passées à la fonction
+ *
+ * Description: Affiche des chaînes données en arguments,
+ * séparées si précisé.
+ */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	
-	unsigned int index;
-	
-	/* Compteur de boucle */
 
-	va_list args;
-	/* Déclaration de la liste d'arguments */
+unsigned int index;
+/* Compteur de boucle */
 
-	va_start(args, n);
-	/* Initialisation de la liste */
+va_list args;
+/* Déclaration de la liste d'arguments */
 
-	for (index = 0; index < n; index++)
-		{
-			char *str = va_arg(args, char*);
+va_start(args, n);
+/* Initialisation de la liste */
 
-			if (str == NULL)
-			{
-    		printf("(nil)");
-			}
+for (index = 0; index < n; index++)
+{
+	char *str = va_arg(args, char*);
+	/* Récupère la chaîne suivante */
 
-			else
-			{
-			printf("%s", str);
-			/* Affiche l'entier suivant dans la liste */
-			}
+	if (str == NULL)
+	{
+		printf("(nil)");
+			/* Affiche (nil) si la chaîne est NULL */
+	}
 
-			if (separator != NULL && index < n - 1)
-			{
-			printf("%s", separator);
-			/* Affiche le séparateur sauf après le dernier nombre */
-			}
-		}
+	else
+	{
+		printf("%s", str);
+			/* Affiche la chaîne */
+	}
 
-	printf("\n");
-	/* Nouvelle ligne à la fin de l'affichage */
+	if (separator != NULL && index < n - 1)
+	{
+		printf("%s", separator);
+			/* Affiche le séparateur si nécessaire */
+	}
+}
 
-	va_end(args);
-	/* Termine l'utilisation de la liste */
+printf("\n");
+/* Nouvelle ligne à la fin de l'affichage */
+
+va_end(args);
+/* Termine l'utilisation de la liste */
 }
