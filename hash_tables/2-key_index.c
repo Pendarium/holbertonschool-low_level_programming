02336ae1561f97e49859168b2e88436a0ad7309e
Pendarium <thomas.sorli21@gmail.com>
2025-08-03 03:41:16 +0200

    kay and betty

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,6 +1,14 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - computes the index of a key in a hash table
+ * @key: the key to compute the index for
+ * @size: the size of the hash table array
+ * Return: the index at which the key/value pair should be stored
+ * in the array of the hash table
+ */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	
+	return (hash_djb2(key) % size);
 }
